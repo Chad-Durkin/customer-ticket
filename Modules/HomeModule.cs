5d8cbd7bfb5b0f7ea989df14be16ed57c55e9dae
Lawrence Eby and Melvin Gruschow <lawrence+melvin@>
2017-03-06 15:50:09 -0800

    Add basic front end for adding tickets

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,11 +9,35 @@ namespace Ticketizer
     {
         public HomeModule()
         {
+            //index
             Get["/"] = _ => {
-              return View["index.cshtml", ModelMaker()];
+                return View["index.cshtml", ModelMaker()];
             };
+            //add dept
+            Post["/add-department"] = _ => {
+                Department newDepartment = new Department(Request.Form["department-name"]);
+                newDepartment.Save();
+
+                return View["index.cshtml", ModelMaker()];
+            };
+            //new ticket form
+            Get["/new-ticket"] = _ => {
+                return View["new-ticket", ModelMaker()];
+            };
+            //new ticket creation
+            Post["/new-ticket"] = _ => {
+                User newUser = new User(Request.Form["user-name"], Request.Form["user-address"], Request.Form["user-phone"], Request.Form["user-email"]);
+                newUser.Save();
+
+                Ticket testTicket = new Ticket(DateTime.Now, Request.Form["ticket-product"], Request.Form["ticket-description"], Request.Form["department-id"], newUser.GetId(), Request.Form["severity"]);
+                testTicket.Save();
+
+                return View["index.cshtml", ModelMaker()];
+            };
+
         }
 
+        // Model Maker
         public Dictionary<string, object> ModelMaker()
         {
             Dictionary<string, object> model = new Dictionary<string, object>{

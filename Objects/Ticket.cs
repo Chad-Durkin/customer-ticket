f8ce51be670e972ff3324f10e922c844dc1ea40d
Chad Durkin and grinil khanna <chad+grinil@>
2017-03-06 09:09:38 -0800

    updated Ticket.cs constructor

--- Objects/Ticket.cs
+++ Objects/Ticket.cs
@@ -14,6 +14,21 @@ namespace Ticketizer
         private string _phone;
         private string _email;
         private string _product;
+        private string _severity;
+        private string _description;
 
+        public Ticket(tickNumber, departmentId, name, address, phone, email, product, severity, description, id = 0)
+        {
+          _id;
+          _ticketNumber;
+          _departmentId;
+          _name;
+          _address;
+          _phone;
+          _email;
+          _product;
+          _severity;
+          _description;
+        }
     }
 }

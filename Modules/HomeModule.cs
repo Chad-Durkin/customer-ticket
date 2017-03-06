c54429cd7d538fee7e65e0427ab1de66a362c48a
Lawrence Eby and Melvin Gruschow <lawrence+melvin@>
2017-03-06 14:57:33 -0800

    Add initial home page

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,8 +10,19 @@ namespace Ticketizer
         public HomeModule()
         {
             Get["/"] = _ => {
-              return View["index.cshtml"];
+              return View["index.cshtml", ModelMaker()];
             };
+        }
+
+        public Dictionary<string, object> ModelMaker()
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>{
+                {"Departments", Department.GetAll()},
+                {"Admins", Admin.GetAll()},
+                {"Tickets", Ticket.GetAll()}
+            };
+
+            return model;
         }
     }
 }

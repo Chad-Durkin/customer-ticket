10f0a3d790b49902c27c0917924f139204d26331
Lawrence Eby and Melvin Gruschow <lawrence+melvin@>
2017-03-06 09:49:47 -0800

    add notes getters, setters and constructor

--- Objects/Notes.cs
+++ Objects/Notes.cs
@@ -7,6 +7,47 @@ namespace Ticketizer
     public class Note
     {
         private int _id;
+        private DateTime _dateCreated;
+        private int _adminId;
+        private int _ticketId;
         private string _text;
     }
+
+// begin Constructors and Getters
+    public Note(DateTime dateCreated, int adminId, int ticketId, string text, int id = 0)
+    {
+        _id = id;
+        _dateCreated = dateCreated;
+        _adminId = adminId;
+        _ticketId = ticketId;
+        _text = text;
+    }
+
+    public int GetId()
+    {
+        return _id;
+    }
+
+    public DateTime GetDateCreated()
+    {
+        return _dateCreated;
+    }
+
+    public int GetAdminId()
+    {
+        return _adminId;
+    }
+
+    public int GetTicketId()
+    {
+        return _ticketId;
+    }
+
+    public string GetText()
+    {
+        return _text;
+    }
+// end Constructors and Getters
+
+
 }
